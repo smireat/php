1b95d8d35f1a9f4b65a53f9fbcb7634fe2c993d6
smireat <s_mireat@naver.com>
2016-01-13 13:57:57 +0900

    #4 css 코드 추가

--- index.php
+++ index.php
@@ -1,7 +1,35 @@
+<style>
+ b {
+  color:red;
+  font-weight:normal;
+  background:#000000;
+ }
+
+ #hellobox {
+  width:500px;
+  height:300px;
+  border:1px solid #ff0000;
+  padding:10px; margin:10px; position:absolute;
+  top:20px; right:20px; 
+}
+
+.nicename {
+  font-weight:bold;
+  font-size:20px;
+  text-decoration:underline;
+  cursor:pointer;
+}
+
+
+</style>
+
+<div id="hellobox">
+ dkssudgktpdy~:
+</div>
 <?php
 
 for( $i=0 ; $i < 10000 ;  $i++){
- echo $i."Taerim </br>";
+ echo $i."<b>hi<span class='nicename'>Tearim</span></br></b>";
 }
 
 ?>
